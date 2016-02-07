d6f78deaa21311b45e158a075adbac908ff38d29
irene robb <email": null>
2016-02-08 12:31:06 +1300

    depolyment whatnot

--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 module.exports =  {
 
   development: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
@@ -13,7 +13,7 @@ module.exports =  {
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
@@ -25,7 +25,7 @@ module.exports =  {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
